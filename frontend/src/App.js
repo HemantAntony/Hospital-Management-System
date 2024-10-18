5a0a37e88b4b3ab9251dfc0620cfe99cacb17868
Hemant Antony <88136054+HemantAntony@users.noreply.github.com>
2024-10-18 16:17:17 +0530

    Added Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from "./pages/Home"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+import NavBar from '../components/NavBar';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Home page</h1>
+            <h3>Is this cool?</h3>
+        </div>
+    )
+}
